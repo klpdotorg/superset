7ba58e0750648976cb68bd34f311bfb4417d9116
shivangi <shivangi@klp.org.in>
2018-10-25 22:06:14 +0530

    added yearmonth and combined it into 1 table

--- scripts/import_supersetdata.py
+++ scripts/import_supersetdata.py
@@ -19,11 +19,12 @@ connectionstring = "dbname=%s user=%s password=%s" % (database,user,passwd)
 conn = psycopg2.connect(connectionstring)
 cursor = conn.cursor()
 
-sqlselect = "select distinct state,district,block,cluster,institution_name,institution_id,gp,qgid,class,qname,qid,sequence,sum(yescount),sum(numcount),village,gender from (select distinct b0.name as state, b1.name as district, b2.name as block, b3.name as cluster, s.name as institution_name, s.id as institution_id, eb.const_ward_name as gp, qg.id as qgid, qg.name as class, q.question_text as qname, q.id as qid, qgq.sequence as sequence, sum(case ans.answer when 'Yes' then 1 when '1' then 1 when 'No' then 0 when '0' then 0 else 0 end) as yescount, count(ans.answer) as numcount, case when s.village is null then '' else s.village end as village, case when lower(ans1.answer) like 'femal%' then 'Female' when lower(ans1.answer) like  'mal%' then 'Male' else 'Male' end gender from assessments_answergroup_institution ag inner join assessments_answerinstitution ans1 on (ag.id=ans1.answergroup_id and ans1.question_id=291), boundary_boundary b0, boundary_boundary b1, boundary_boundary b2, boundary_boundary b3, schools_institution s, boundary_electionboundary eb, assessments_answerinstitution ans, assessments_questiongroup qg, assessments_question q, assessments_questiongroup_questions qgq where ag.id = ans.answergroup_id and ag.questiongroup_id = qg.id and ans.question_id = q.id and ag.institution_id = s.id and s.admin0_id = b0.id and s.admin1_id = b1.id and s.admin2_id = b2.id and s.admin3_id = b3.id and s.gp_id = eb.id and qg.survey_id = 2 and q.question_text not in ('Gender', 'Class visited') and qgq.questiongroup_id = qg.id and qgq.question_id = q.id and qg.survey_id="+survey_id+" group by b0.name, b1.name, b2.name, b3.name, s.village, s.name, s.id, eb.const_ward_name, qg.id, qg.name, ans.answer, q.question_text, qgq.sequence, q.id,ans1.answer order by sequence)data group by state,district,block,cluster,institution_name,institution_id,gp,qgid,class,qname,qid,sequence,village,gender"
+sqlselect = "select distinct state,district,block,cluster,institution_name,institution_id,gp,qgid,class,qname,qid,sequence,sum(yescount),sum(numcount),village,gender,yearmonth from (select distinct b0.name as state, b1.name as district, b2.name as block, b3.name as cluster, s.name as institution_name, s.id as institution_id, eb.const_ward_name as gp, qg.id as qgid, qg.name as class, q.question_text as qname, q.id as qid, qgq.sequence as sequence, sum(case ans.answer when 'Yes' then 1 when '1' then 1 when 'No' then 0 when '0' then 0 else 0 end) as yescount, count(ans.answer) as numcount, case when s.village is null then '' else s.village end as village, case when lower(ans1.answer) like 'femal%' then 'Female' when lower(ans1.answer) like  'mal%' then 'Male' else 'Male' end gender,to_char(ag.date_of_visit,'YYYYMM')::int as yearmonth from assessments_answergroup_institution ag inner join assessments_answerinstitution ans1 on (ag.id=ans1.answergroup_id and ans1.question_id=291), boundary_boundary b0, boundary_boundary b1, boundary_boundary b2, boundary_boundary b3, schools_institution s, boundary_electionboundary eb, assessments_answerinstitution ans, assessments_questiongroup qg, assessments_question q, assessments_questiongroup_questions qgq where ag.id = ans.answergroup_id and ag.questiongroup_id = qg.id and ans.question_id = q.id and ag.institution_id = s.id and s.admin0_id = b0.id and s.admin1_id = b1.id and s.admin2_id = b2.id and s.admin3_id = b3.id and s.gp_id = eb.id and qg.survey_id = 2 and q.question_text not in ('Gender', 'Class visited') and qgq.questiongroup_id = qg.id and qgq.question_id = q.id group by b0.name, b1.name, b2.name, b3.name, s.village, s.name, s.id, eb.const_ward_name, qg.id, qg.name, ans.answer, q.question_text, qgq.sequence, q.id,ans1.answer , yearmonth order by sequence)data group by state,district,block,cluster,institution_name,institution_id,gp,qgid,class,qname,qid,sequence,village,gender, yearmonth"
 
 data = {}
 cursor.execute(sqlselect,)
-qgids = {}
+
+
 for row in cursor.fetchall():
     state = row[0]
     district = row[1]
@@ -41,74 +42,62 @@ for row in cursor.fetchall():
     numcount = row[13]
     village = row[14].replace("'","''")
     gender = row[15]
-    if qgid in qgids:
-        if sequence not in qgids[qgid]["sequences"]:
-            qgids[qgid]["sqlcreate"] = qgids[qgid]["sqlcreate"]+", qname"+sequence+" VARCHAR (50), qid"+sequence+" integer,  yescount"+sequence+" integer,  numcount"+sequence+" integer"
-            qgids[qgid]["sequences"][sequence] = 1
-    else:
-        qgids[qgid] = {"sqlcreate":"","sequences":{}}
-        qgids[qgid]["sqlcreate"] = "create table superset_"+qgid+" (state VARCHAR (50), district VARCHAR (50), block VARCHAR (50), cluster VARCHAR (50), village VARCHAR (100), institution_name VARCHAR (100), institution_id integer, gp VARCHAR (100), class VARCHAR (50), gender VARCHAR (50)" 
-        qgids[qgid]["sqlcreate"] = qgids[qgid]["sqlcreate"]+", qname"+sequence+" VARCHAR (50), qid"+sequence+" integer, yescount"+sequence+" integer,  numcount"+sequence+" integer"
-        qgids[qgid]["sequences"][sequence] = 1
+    yearmonth = row[16]
         
     if qgid in data:
-        if institution_id in data[qgid]:
-            if qg_name in data[qgid][institution_id]["qgs"]:
-                if gender in data[qgid][institution_id]["qgs"][qg_name]:
-                    if sequence in data[qgid][institution_id]["qgs"][qg_name][gender]["questions"]:
-                        data[qgid][institution_id]["qgs"][qg_name][gender]["questions"][sequence]["yescount"] += yescount
-                        data[qgid][institution_id]["qgs"][qg_name][gender]["questions"][sequence]["numcount"] += numcount 
-                        data[qgid][institution_id]["qgs"][qg_name][gender]["total_numcount"] += numcount 
-                        data[qgid][institution_id]["qgs"][qg_name][gender]["total_correctcount"] += yescount 
+        if institution_id in data[qgid]["institutions"]:
+            if yearmonth in data[qgid]["institutions"][institution_id]["yearmonths"]:
+                if gender in data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth]:
+                    if sequence in data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender]["questions"]:
+                        data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender]["questions"][sequence]["yescount"] += yescount
+                        data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender]["questions"][sequence]["numcount"] += numcount 
+                        data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender]["total_numcount"] += numcount 
+                        data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender]["total_correctcount"] += yescount 
                     else:
-                        data[qgid][institution_id]["qgs"][qg_name][gender]["questions"][sequence] = {"qid":qid, "qname":qname, "yescount":yescount, "numcount":numcount}
-                        data[qgid][institution_id]["qgs"][qg_name][gender]["total_numcount"] += numcount 
-                        data[qgid][institution_id]["qgs"][qg_name][gender]["total_correctcount"] += yescount 
+                        data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender]["questions"][sequence] = {"qid":qid, "qname":qname, "yescount":yescount, "numcount":numcount}
+                        data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender]["total_numcount"] += numcount 
+                        data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender]["total_correctcount"] += yescount 
                 else:
-                    data[qgid][institution_id]["qgs"][qg_name][gender] = {"questions": {},"total_numcount":0, "total_correctcount":0} 
-                    data[qgid][institution_id]["qgs"][qg_name][gender]["total_numcount"] += numcount 
-                    data[qgid][institution_id]["qgs"][qg_name][gender]["total_correctcount"] += yescount 
-                    data[qgid][institution_id]["qgs"][qg_name][gender]["questions"][sequence] = {"qid":qid, "qname":qname, "yescount":yescount, "numcount":numcount}
+                    data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender] = {"questions": {},"total_numcount":numcount, "total_correctcount":yescount} 
+                    data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender]["questions"][sequence] = {"qid":qid, "qname":qname, "yescount":yescount, "numcount":numcount}
             else:
-                data[qgid][institution_id]["qgs"][qg_name] = {}
-                data[qgid][institution_id]["qgs"][qg_name][gender] = {"questions": {},"total_numcount":0, "total_correctcount":0} 
-                data[qgid][institution_id]["qgs"][qg_name][gender]["total_numcount"] += numcount 
-                data[qgid][institution_id]["qgs"][qg_name][gender]["total_correctcount"] += yescount 
-                data[qgid][institution_id]["qgs"][qg_name][gender]["questions"][sequence] = {"qid":qid, "qname":qname, "yescount":yescount, "numcount":numcount}
+                data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth] = {}
+                data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender] = {"questions": {},"total_numcount":numcount, "total_correctcount":yescount} 
+                data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth][gender]["questions"][sequence] = {"qid":qid, "qname":qname, "yescount":yescount, "numcount":numcount}
         else:
-            data[qgid][institution_id] = {"state":state, "district": district, "block": block, "cluster":cluster, "village": village, "institution_name":institution_name, "institution_id":institution_id, "gp": gp, "qgs" :{}}
-            data[qgid][institution_id]["qgs"][qg_name] = {gender:{"questions": {sequence: {"qid":qid, "qname":qname, "yescount":yescount, "numcount":numcount}},"total_numcount": 0, "total_correctcount": 0}}
-            data[qgid][institution_id]["qgs"][qg_name][gender]["total_numcount"] += numcount 
-            data[qgid][institution_id]["qgs"][qg_name][gender]["total_correctcount"] += yescount 
+            data[qgid]["institutions"][institution_id] = {"state":state, "district": district, "block": block, "cluster":cluster, "village": village, "institution_name":institution_name, "institution_id":institution_id, "gp": gp, "yearmonths" :{}}
+            data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth] = {gender:{"questions": {sequence: {"qid":qid, "qname":qname, "yescount":yescount, "numcount":numcount}},"total_numcount": numcount, "total_correctcount": yescount}}
     else:
-        data[qgid]={}
-        data[qgid][institution_id] = {"state":state, "district": district, "block": block, "cluster":cluster, "village": village, "institution_name":institution_name, "institution_id":institution_id, "gp": gp, "qgs" :{}}
-        data[qgid][institution_id]["qgs"][qg_name] = {gender:{"questions": {sequence: {"qid":qid, "qname":qname, "yescount":yescount, "numcount":numcount}}, "total_numcount": 0, "total_correctcount": 0}}
-        data[qgid][institution_id]["qgs"][qg_name][gender]["total_numcount"] += numcount 
-        data[qgid][institution_id]["qgs"][qg_name][gender]["total_correctcount"] += yescount 
+        data[qgid]={"qgname":qg_name,"institutions":{}}
+        data[qgid]["institutions"][institution_id] = {"state":state, "district": district, "block": block, "cluster":cluster, "village": village, "institution_name":institution_name, "institution_id":institution_id, "gp": gp, "yearmonths" :{}}
+        data[qgid]["institutions"][institution_id]["yearmonths"][yearmonth] = {gender:{"questions": {sequence: {"qid":qid, "qname":qname, "yescount":yescount, "numcount":numcount}}, "total_numcount": numcount, "total_correctcount": yescount}}
        
 
 
-for qgid in qgids:
-     sqlcreate = qgids[qgid]["sqlcreate"]
-     sqlcreate = sqlcreate+", total_correctcount integer, total_numcount integer);"
-     cursor.execute(sqlcreate)
-     conn.commit()
+sqlcreate = "create table superset_"+survey_id+" (state VARCHAR (50), district VARCHAR (50), block VARCHAR (50), cluster VARCHAR (50), village VARCHAR (100), institution_name VARCHAR (100), institution_id integer, gp VARCHAR (100), class VARCHAR (50), yearmonth integer, gender VARCHAR (50), qgid integer"
+for num in range(1,21):
+    sqlcreate = sqlcreate+", qname"+str(num)+" VARCHAR(100), qid"+str(num)+" integer, yescount"+str(num)+" integer, numcount"+str(num)+" integer"
+sqlcreate = sqlcreate+", total_correctcount integer, total_numcount integer);"
+cursor.execute(sqlcreate)
+conn.commit()
 
 print("created table")
 print("starting data import")
 
 
 for qgid in data:
-    for institution_id in data[qgid]:
-        init_sqlinsert = "insert into superset_"+qgid+" values("
-        init_sqlinsert = init_sqlinsert +"'"+data[qgid][institution_id]["state"]+"','"+data[qgid][institution_id]["district"]+"','"+data[qgid][institution_id]["block"]+"','"+data[qgid][institution_id]["cluster"]+"','"+data[qgid][institution_id]["village"]+"','"+data[qgid][institution_id]["institution_name"]+"',"+str(institution_id)+",'"+data[qgid][institution_id]["gp"]+"'"
-        for qg in data[qgid][institution_id]["qgs"]:
-            for gender in data[qgid][institution_id]["qgs"][qg]:
-                sqlinsert = init_sqlinsert+",'"+qg+"','"+gender+"'"
-                for sequence in data[qgid][institution_id]["qgs"][qg][gender]["questions"]:
-                    sqlinsert = sqlinsert+",'"+data[qgid][institution_id]["qgs"][qg][gender]["questions"][sequence]["qname"]+"',"+str(data[qgid][institution_id]["qgs"][qg][gender]["questions"][sequence]["qid"])+","+str(data[qgid][institution_id]["qgs"][qg][gender]["questions"][sequence]["yescount"])+","+str(data[qgid][institution_id]["qgs"][qg][gender]["questions"][sequence]["numcount"])
-                sqlinsert = sqlinsert+","+str(data[qgid][institution_id]["qgs"][qg][gender]["total_correctcount"])+","+str(data[qgid][institution_id]["qgs"][qg][gender]["total_numcount"])+");"
-            cursor.execute(sqlinsert)
+    for institution_id in data[qgid]["institutions"]:
+        init_sqlinsert = "insert into superset_"+survey_id+" values("
+        institution_info = data[qgid]["institutions"][institution_id]
+        init_sqlinsert = init_sqlinsert +"'"+institution_info["state"]+"','"+institution_info["district"]+"','"+institution_info["block"]+"','"+institution_info["cluster"]+"','"+institution_info["village"]+"','"+institution_info["institution_name"]+"',"+str(institution_id)+",'"+institution_info["gp"]+"','"+data[qgid]['qgname']+"'"
+        for yearmonth in institution_info["yearmonths"]:
+            yearmonth_info = institution_info["yearmonths"][yearmonth]
+            for gender in yearmonth_info:
+                sqlinsert = init_sqlinsert+","+str(yearmonth)+",'"+gender+"',"+qgid
+                for sequence in yearmonth_info[gender]["questions"]:
+                    question_info = yearmonth_info[gender]["questions"][sequence]
+                    sqlinsert = sqlinsert+",'"+question_info["qname"]+"',"+str(question_info["qid"])+","+str(question_info["yescount"])+","+str(question_info["numcount"])
+                sqlinsert = sqlinsert+","+str(yearmonth_info[gender]["total_correctcount"])+","+str(yearmonth_info[gender]["total_numcount"])+");"
+                cursor.execute(sqlinsert)
     
 conn.commit()
